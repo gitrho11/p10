d5d1bc700b04a68b5a5974b47cd91d121f3546ab
maco green <gitrho11@yahoo.com>
2009-03-02 16:25:14 -0800

    hello world

---
+++ gaddem.sh
@@ -0,0 +1,14 @@
+
+git add         gaddem.sh
+git add         notes2009_0225.txt
+git add         notes2009_0226.txt
+git add         notes2009_0227.txt
+git add         notes2009_0302.txt
+git add         rhomobile_tutorial01.pdf
+git add         rhomobile_tutorial21.pdf
+git add         rhomobile_tutorial22.pdf
+git add         rhomobile_tutorial23.pdf
+
+git commit 'hello world'
+git push origin master
+
